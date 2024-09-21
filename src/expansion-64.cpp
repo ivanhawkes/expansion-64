663d6c1be606cc72098ec03776fe6bce366ead52
Ivan Hawkes <ivan.hawkes@gmail.com>
2024-09-21 23:55:03 +1000

    Minor changes.

--- src/expansion-64.cpp
+++ src/expansion-64.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "hardware/clocks.h"
 #include "hardware/i2c.h"
 #include "hardware/timer.h"
-#include "hardware/clocks.h"
 #include "hardware/uart.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -11,12 +11,6 @@
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
-
 
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
@@ -31,42 +25,26 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 
 int main()
 {
-    stdio_init_all();
+	stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+	// I2C Initialisation. Using it at 400Khz.
+	i2c_init(I2C_PORT, 400 * 1000);
+	gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+	gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+	gpio_pull_up(I2C_SDA);
+	gpio_pull_up(I2C_SCL);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+	// Set up our UART
+	uart_init(UART_ID, BAUD_RATE);
+	gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+	gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+	// Send out a string, with CR/LF conversions
+	uart_puts(UART_ID, " Hello, UART!\n");
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+	while (true)
+	{
+		printf("Hello, world!\n");
+		sleep_ms(1000);
+	}
 }
